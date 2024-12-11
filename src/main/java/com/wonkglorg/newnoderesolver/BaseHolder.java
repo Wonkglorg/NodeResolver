762f367e5f2e257002a19c8d8207030b5aad4a3e
P10209 <Wonkglorg.18@gmail.com>
2024-12-11 14:47:52 +0100

    Trying Holders

--- src/main/java/com/wonkglorg/newnoderesolver/BaseHolder.java
+++ src/main/java/com/wonkglorg/newnoderesolver/BaseHolder.java
@@ -4,6 +4,7 @@ import java.util.function.Consumer;
 
 public abstract class BaseHolder<T> {
 	protected final T value;
+	//todo:jmd on runtime generate the different holders for each case? since alot can be extracted to annotations
 
 	protected BaseHolder(T value) {
 		this.value = value;
